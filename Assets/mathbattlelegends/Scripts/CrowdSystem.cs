368acce0d46a587fb784fabdbdeddf38277b8c7f
Gesare-n <nicolegesare00@gmail.com>
2025-05-26 21:19:02 +0300

    crowdsystemcode

---
+++ Assets/mathbattlelegends/Scripts/CrowdSystem.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CrowdSystem : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private float radius;
+    [SerializeField] private float angle;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlaceRunners();
+        
+    }
+
+    private void PlaceRunners()
+    {
+      for (int i = 0; i < transform.childCount; i++)
+        {
+            Vector3 childLocalPosition = GetRunnerLocalPosition(i);
+            transform.GetChild(i).localPosition = childLocalPosition; 
+        }
+    }
+
+    private Vector3 GetRunnerLocalPosition(int index)
+    {
+        float x = radius * Mathf.Sqrt(index) * Mathf.Cos(Mathf.Deg2Rad * index * angle);
+        float z = radius * Mathf.Sqrt(index) * Mathf.Sin(Mathf.Deg2Rad * index * angle);
+
+        return new Vector3(x, 0, z);
+
+    }
+}
